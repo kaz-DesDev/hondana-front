bf8f2e1b9eaba4232a8624b908d659189c749916
kaz-DesDev <kaz.desdev@gmail.com>
2020-12-06 17:47:49 +0900

    Cypressテストの追加

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,26 @@
 module.exports = {
-    roots: ["<rootDir>"],
-    moduleFileExtensions: ["js", "ts", "tsx", "json"],
-    testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
-    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
-    transform: {
-      "^.+\\.(ts|tsx)$": "babel-jest",
+  preset: "ts-jest",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["js", "ts", "jsx, tsx", "json"],
+  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "babel-jest",
+  },
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^(pages|components)/(.+)": "<rootDir>/src/$1/$2",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/src/__test__/__mocks__/fileMock.js",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        jsx: "react",
+      },
     },
-    watchPlugins: [
-      "jest-watch-typeahead/filename",
-      "jest-watch-typeahead/testname",
-    ],
-    moduleNameMapper: {
-      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-      "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
-    },
+  },
 };
