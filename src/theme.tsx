baa8524a5ebec1d923e05c86c9cf4375952d1e56
kaz-DesDev <kaz.desdev@gmail.com>
2020-12-07 20:34:21 +0900

    テーマカラーを反映

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,10 +5,10 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#FFC200",
     },
     secondary: {
-      main: "#19857b",
+      main: "#E43F16",
     },
     error: {
       main: red.A400,
